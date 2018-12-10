e4f34f27928b6f4aa2dc95b7a8654dd377e696e6
uwjo <uwjoy1@gmail.com>
2018-12-10 11:58:48 +0200

    pushing to develop branch

--- src/Component/auth/Destinationlist.js
+++ src/Component/auth/Destinationlist.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
 
-export default class Destination extends Component {
+class Destination extends Component {
     render() {
         return (
             <div>
+                <div className="Signup">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-8 m-auto">
+                                <h1 className="display-4 text-center">Sign Up</h1>
+                                <p className="lead text-center">On Safe Bus</p>
+                                <form on Submit={this.onSubmit}></form>
+                                <form action="create-profile.html">
+                                    <div className="form-group">
+                                        <input type="text" className="form-control form-control-lg" placeholder="Name of Transportation Company" name="name" value={this.state.name} onChange={this.onChange} />
+
+                                    </div>
+
+
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
 
             </div>
+
         )
     }
 }
 
+export default Destination;
